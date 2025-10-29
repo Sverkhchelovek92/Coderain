24022d82b494b80bb159f78ef550dd3ff0afe06b
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-10-29 13:43:06 +0300

    falling letter

--- index.js
+++ index.js
@@ -9,5 +9,34 @@ function resizeCanvas() {
 resizeCanvas()
 window.addEventListener('resize', resizeCanvas)
 
-ctx.fillStyle = '#00ff00'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+const fontSize = 20
+const symbols =
+  'アァイィウヴエェオカキクケコサシスセソタチツテトナニヌネノ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+let y = 0
+let speed = 2
+
+function draw() {
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  //Font
+  ctx.fillStyle = '#0F0'
+  ctx.font = `${fontSize}px monospace`
+
+  //Random Symbol
+  const char = symbols.charAt(Math.floor(Math.random() * symbols.length))
+
+  const x = canvas.width / 2
+  ctx.fillText(char, x, y)
+
+  y += speed
+
+  if (y > canvas.height) {
+    y = 0
+  }
+
+  requestAnimationFrame(draw)
+}
+
+draw()
