24ef7effad0cd46407519b773b723a897ba84a18
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-10-30 13:57:00 +0300

    matrix code rain v 1.0

--- index.js
+++ index.js
@@ -28,49 +28,74 @@ function randomChar() {
   return symbols.charAt(Math.floor(Math.random() * symbols.length))
 }
 
+// stream class
+class Stream {
+  constructor(x) {
+    this.x = x
+    this.reset()
+  }
+
+  reset() {
+    this.length = Math.floor(10 + Math.random() * 20)
+    this.y = Math.random() * -canvas.height
+    this.trail = []
+    this.speed = 80 + Math.random() * 100
+  }
+
+  step() {
+    // add new char to the top
+    this.trail.unshift({ char: randomChar(), alpha: 1.0 })
+
+    // regulate transparency of old chars
+    for (const item of this.trail) item.alpha *= 0.85
+
+    // delete old chars
+    if (this.trail.length > this.length) this.trail.pop()
+
+    this.y += fontSize
+
+    if (this.y > canvas.height + this.length * fontSize) {
+      this.x = Math.random() * canvas.width
+      this.reset()
+    }
+  }
+
+  draw() {
+    for (let i = 0; i < this.trail.length; i++) {
+      const item = this.trail[i]
+      const y = this.y - i * fontSize
+
+      if (y < -fontSize || y > canvas.height) continue
+
+      if (i === 0) {
+        // steam head
+        ctx.fillStyle = '#ccffcc'
+        ctx.shadowColor = '#aaffaa'
+        ctx.shadowBlur = 12
+        ctx.fillText(item.char, this.x, y)
+        ctx.shadowBlur = 0
+      } else {
+        // steam trail
+        ctx.fillStyle = `rgba(0, 255, 0, ${item.alpha.toFixed(2)})`
+        ctx.fillText(item.char, this.x, y)
+      }
+    }
+  }
+}
+
+const streamCount = Math.floor(window.innerWidth / 30)
+let streams = Array.from(
+  { length: streamCount },
+  () => new Stream(Math.random() * canvas.width)
+)
+
 function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-  // Adding new char
-  trail.unshift({ char: randomChar(), alpha: 1.0 })
-
-  // Draw every char
-  for (let i = 0; i < trail.length; i++) {
-    const item = trail[i]
-    const y = headY - i * fontSize
-
-    // Ignore symbols out of the borders
-    if (y < -fontSize) continue
-    if (y > canvas.height) break
-
-    // Trail style
-    if (i === 0) {
-      ctx.fillStyle = '#ccffcc'
-      ctx.shadowColor = '#0f0'
-      ctx.shadowBlur = 12 // glow effect
-      ctx.fillText(item.char, columnX, y)
-      ctx.shadowBlur = 0
-    } else {
-      // other symbols
-      const brightness = Math.max(item.alpha, 0.1)
-      ctx.fillStyle = `rgba(0, 255, 0, ${brightness})`
-      ctx.fillText(item.char, columnX, y)
-    }
-
-    item.alpha *= 0.85
-  }
-
-  // Deleting old symbols
-  trail = trail.filter((item) => item.alpha > 0.05)
-
-  // Moving head
-  headY += fontSize
-
-  // Restart if at the bottom
-  if (headY > canvas.height + fontSize * 2) {
-    headY = 0
-    trail = []
+  for (const stream of streams) {
+    stream.step()
+    stream.draw()
   }
 
   setTimeout(() => requestAnimationFrame(draw), speed)
